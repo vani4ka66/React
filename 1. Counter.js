c230cb40b0003c2309845e962c48a65b758324da
Vania Dimitrova <vani4ka66@abv.bg>
2020-01-13 13:07:41 +0200

    Create 1. Counter.js

---
+++ 1. Counter.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+
+
+class App extends React.Component {
+    constructor(){
+        super()
+
+        this.state = {
+            counter: 0,
+            name: 'Pesho'
+        }
+    }
+
+    componentDidMount(){
+        setInterval(() => {
+            this.setState({
+                counter: this.state.counter + 1
+            })
+        }, 1000)
+    }
+
+    render(){
+        return(
+            <div className="App">
+                <header className="App-header">
+                    {this.state.name}
+                    <br></br>
+                {this.state.counter}
+                </header>
+            </div>
+        )
+    }
+}
+
+export default App;
+// export {App}
